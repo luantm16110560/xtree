073faf2270d91525774cc25a1b1a558b744ab572
Luan Truong Minh <luan.tm@districteightdesign.com>
2020-11-20 10:06:40 +0700

    update

--- models/ir_action.py
+++ models/ir_action.py
@@ -5,4 +5,4 @@ from odoo import fields, models
 class ActWindowView(models.Model):
     _inherit = 'ir.actions.act_window.view'
 
-    view_mode = fields.Selection(selection_add=[('xtree', "X Tree")], ondelete={'xtree': 'cascade'})
+    view_mode = fields.Selection(selection_add=[('xtree', "XTree")], ondelete={'xtree': 'cascade'})
